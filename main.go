09e5f340a72954249c79be5f25cc30144943d46b
Liam Garvie <liamgarvie@192.168.1.189>
2023-10-05 22:26:52 +0100

    inital response handler

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
+import "net/http"
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, quote.Go(), r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
 }
