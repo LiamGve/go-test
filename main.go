04cd4ee7f0a18fb4ebc4935ffbee4ec22706e23e
Liam Garvie <liamgarvie@192.168.1.189>
2023-10-05 22:42:02 +0100

    inital response handler

--- main.go
+++ main.go
@@ -1,14 +1,50 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	ConfigRuntime()
+	StartWorkers()
+	StartGin()
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, quote.Go(), r.URL.Path)
-	})
+// ConfigRuntime sets the number of operating system threads.
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	fmt.Printf("Running with %d CPUs\n", nuCPU)
+}
 
-	http.ListenAndServe(":443", nil)
+// StartWorkers start starsWorker by goroutine.
+func StartWorkers() {
+	go statsWorker()
+}
+
+// StartGin starts gin web server with setting router.
+func StartGin() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+	router.Use(rateLimit, gin.Recovery())
+	router.LoadHTMLGlob("resources/*.templ.html")
+	router.Static("/static", "resources/static")
+	router.GET("/", index)
+	router.GET("/room/:roomid", roomGET)
+	router.POST("/room-post/:roomid", roomPOST)
+	router.GET("/stream/:roomid", streamRoom)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+        log.Panicf("error: %s", err)
+	}
 }
